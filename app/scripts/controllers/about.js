f6091ecf9f1d5f68968422d19d390b43fa92024c
barral-v <valentin.barral@epitech.eu>
2016-03-05 15:43:34 +0100

    Starting Web App 2

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name feedMeWebApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the feedMeWebApp
+ */
+angular.module('feedMeWebApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
