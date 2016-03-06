7197289e489e3640592491cd59bd94b6aa458e32
barral-v <valentin.barral@epitech.eu>
2016-03-06 11:37:56 +0100

    change background color

--- app/scripts/controllers/loginroute.js
+++ app/scripts/controllers/loginroute.js
@@ -7,11 +7,22 @@
  * # LoginrouteCtrl
  * Controller of the feedMeWebApp
  */
-angular.module('feedMeWebApp')
-  .controller('LoginrouteCtrl', function () {
+var app = angular.module('feedMeWebApp');
+ 
+app.controller('LoginrouteCtrl', function ($scope) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+    // function to submit the form after all validation has occurred            
+    $scope.submitForm = function() {
+
+        // check to make sure the form is completely valid
+        if ($scope.userForm.$valid) {
+            alert('our form is amazing');
+        }
+
+    };
   });
+
