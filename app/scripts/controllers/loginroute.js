83ba8a7857ef10e3588a1bb47209d58412564705
barral-v <valentin.barral@epitech.eu>
2016-03-12 12:17:09 +0100

    plus qu'a add l'appel a l'api

--- app/scripts/controllers/loginroute.js
+++ app/scripts/controllers/loginroute.js
@@ -17,7 +17,8 @@ app.controller('LoginrouteCtrl', function ($scope) {
     ];
     // function to submit the form after all validation has occurred            
     $scope.submitForm = function(isValid) {
-
+		
+		$scope.submitted = true;
         // check to make sure the form is completely valid
         if (isValid) {
             alert('our form is amazing');
