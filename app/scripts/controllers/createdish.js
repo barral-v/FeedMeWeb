377f95d0c77267ada8ef825cc69724c825387424
barral-v <valentin.barral@epitech.eu>
2016-03-26 11:18:09 +0100

    forgot some files

---
+++ app/scripts/controllers/createdish.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name feedMeWebApp.controller:LoginrouteCtrl
+ * @description
+ * # LoginrouteCtrl
+ * Controller of the feedMeWebApp
+ */
+var app = angular.module('feedMeWebApp');
+ 
+app.controller('LoginrouteCtrl', function ($scope) {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+    // function to submit the form after all validation has occurred            
+  	$scope.submitForm = function(isValid) {
+
+	    // check to make sure the form is completely valid
+	    if (isValid) {
+	      alert('our form is amazing');
+	    }
+
+  	};
+  });
+
---
+++ app/scripts/controllers/feedmemap.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name feedMeWebApp.controller:LoginrouteCtrl
+ * @description
+ * # LoginrouteCtrl
+ * Controller of the feedMeWebApp
+ */
+var app = angular.module('feedMeWebApp');
+ 
+app.controller('MapCtrl', ['uiGmapgoogle-maps', function ($scope) {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+    $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
+  }]);
+
