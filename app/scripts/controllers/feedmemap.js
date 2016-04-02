0aeb93927d9d7d232bc4680485bad8438b5e7988
barral-v <valentin.barral@epitech.eu>
2016-04-02 16:15:39 +0200

    mysteriously functional map

--- app/scripts/controllers/feedmemap.js
+++ app/scripts/controllers/feedmemap.js
@@ -15,18 +15,15 @@ app.controller('MapCtrl', ['$scope', '$http', function ($scope, $http) {
       'AngularJS',
       'Karma'
     ];
-    $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
+    $scope.map = { center: { latitude: 48.831451, longitude: 2.3203403 }, zoom: 15 };
     $scope.initMap = function () {
         var map = new google.maps.Map(document.getElementById('map'), {
           center: {lat: -34.397, lng: 150.644},
           zoom: 8
         });
       }
+    $scope.listMarkers = [
+      {latitude: 48.8307635, longitude: 2.3206993, title: "M1", 'id': 1, 'options': {'label': "MARKER 1"}},
+      {latitude: 48.8310, longitude: 2.32030, title: "M2", 'id': 2, 'options': {'label': "MARKER 2"}}
+    ];
   }]);
-
- function initMap() {
-        var map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -34.397, lng: 150.644},
-          zoom: 8
-        });
-      }
--- app/scripts/controllers/loginroute.js
+++ app/scripts/controllers/loginroute.js
@@ -20,9 +20,14 @@ app.controller('LoginrouteCtrl', function ($scope, $location, $http) {
 	    // check to make sure the form is completely valid
 	    if (isValid) {
         var user = $scope.user;
-        if (user.username == "admin" && user.password == "admin"){
-          $location.path('/map').replace();
-        }
+        var url = 'http://163.5.84.232/WebService/api/Utilisateurs?email=';
+        url += user.username  + '&password=' + user.password;
+        $http({method: 'GET', url: url, headers: { 'Content-Type': 'application/x-www-form-urlencoded' }}).then(function successCallback(response) {
+              console.log(response);
+              $location.path('/map').replace();
+            }, function errorCallback(response) {
+              console.log(response);
+            });
 	    }
 
   	};
