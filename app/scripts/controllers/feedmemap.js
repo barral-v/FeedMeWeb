fc441b515c5e27e1f4d789626f352d6f033cfdc1
barral-v <valentin.barral@epitech.eu>
2016-04-02 17:28:08 +0200

    clickable markers

--- app/scripts/controllers/feedmemap.js
+++ app/scripts/controllers/feedmemap.js
@@ -9,21 +9,21 @@
  */
 var app = angular.module('feedMeWebApp');
  
-app.controller('MapCtrl', ['$scope', '$http', function ($scope, $http) {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+app.controller('MapCtrl', function ($scope, $http, $location) {
+
+    var clickMarker = function(){
+      $location.path('/').replace();
+    };
+
     $scope.map = { center: { latitude: 48.831451, longitude: 2.3203403 }, zoom: 15 };
     $scope.initMap = function () {
         var map = new google.maps.Map(document.getElementById('map'), {
           center: {lat: -34.397, lng: 150.644},
           zoom: 8
         });
-      }
+      };
     $scope.listMarkers = [
-      {latitude: 48.8307635, longitude: 2.3206993, title: "M1", 'icon': ' http://icons.iconarchive.com/icons/fatcow/farm-fresh/32/rainbow-icon.png', 'id': 1, 'options': {labelClass: 'maplabel', labelAnchor:'12 60', 'labelContent': "MARKER 1"}},
-      {latitude: 48.8310, longitude: 2.32030, title: "M2", 'icon': ' http://icons.iconarchive.com/icons/fatcow/farm-fresh/32/rainbow-icon.png', 'id': 2, 'options': {labelClass: 'maplabel', labelAnchor:'12 60', 'labelContent': "MARKER 2"}}
+      {latitude: 48.8307635, longitude: 2.3206993, title: "M1", 'icon': ' http://icons.iconarchive.com/icons/fatcow/farm-fresh/32/rainbow-icon.png', 'id': 1, 'events': {click: clickMarker}, 'options': {labelClass: 'maplabel', labelAnchor:'12 60', 'labelContent': "MARKER 1"}},
+      {latitude: 48.8310, longitude: 2.32030, title: "M2", 'icon': ' http://icons.iconarchive.com/icons/fatcow/farm-fresh/32/rainbow-icon.png', 'id': 2, 'events': {click: clickMarker}, 'options': {labelClass: 'maplabel', labelAnchor:'12 60', 'labelContent': "MARKER 2"}}
     ];
-  }]);
+  });
