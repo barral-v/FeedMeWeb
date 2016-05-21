974cee1ed936bdcf8e06cb12144f5b887d27815b
barral-v <valentin.barral@epitech.eu>
2016-05-21 11:22:44 +0200

    Fix Create Account

--- app/scripts/controllers/createaccount.js
+++ app/scripts/controllers/createaccount.js
@@ -13,7 +13,7 @@ app.controller('CreateaccountCtrl', ['$cookies', '$scope', '$location', '$http',
     // function to submit the form after all validation has occurred            
   	$scope.submitCreateAccount = function(isValid) {
       // check to make sure the form is completely valid
-      if (isValid && ($scope.user.Password == $scope.passwordvalidator)) {
+      if (isValid && ($scope.user.Password == $scope.user.ConfirmPassword)) {
 		    var user = $scope.user;
             var url = 'http://163.5.84.232/WebService/api/Account/Register';
 
@@ -21,23 +21,13 @@ app.controller('CreateaccountCtrl', ['$cookies', '$scope', '$location', '$http',
               method: 'POST',
               url: url,
               data: user,
-              transformRequest: function (obj) {
-                    var str = [];
-                    for (var p in obj)
-                        str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
-                    return str.join("&");
-                }
+              headers: {
+                        'Content-Type': 'application/json;charset=utf-8'
+                    },
             }
 
             $http(request).then(function successCallback(response) {
-              var data = response.data;
-              if (data.access_token){
-                $cookies.put("feedmetoken", data.access_token)
-                $location.path('/map').replace();
-            }
-            else{
-                $scope.requestError = response.statusText
-            }
+                $location.path('/').replace();
         }, function errorCallback(response) {
             console.log(response);
             if (response.statusText == "Not Found"){
