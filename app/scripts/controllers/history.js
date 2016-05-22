ab94c4256b2363f15476bd3078f18874cfb6ae6c
barral-v <valentin.barral@epitech.eu>
2016-05-22 16:22:04 +0200

    History working

--- app/scripts/controllers/history.js
+++ app/scripts/controllers/history.js
@@ -7,11 +7,45 @@
  * # HistoryCtrl
  * Controller of the feedMeWebApp
  */
-angular.module('feedMeWebApp')
-  .controller('HistoryCtrl', '$cookies', '$scope', '$location', '$http', function  ($cookies, $scope, $location, $http) {
-  
-        if (!$cookies.get("feedmetoken")){ 
-            $location.path('/').replace(); 
-        } 
+ angular.module('feedMeWebApp')
+ .controller('HistoryCtrl', ['$cookies', '$scope', '$location', '$http', function  ($cookies, $scope, $location, $http) {
 
-  }]);
+    if (!$cookies.get("feedmetoken")){ 
+        $location.path('/').replace(); 
+    }
+
+    var url = 'http://163.5.84.232/WebService/api/Historic'
+
+    var request = {
+        method: 'GET',
+        url: url,
+        headers: {
+            'Authorization': 'Bearer '+ $cookies.get("feedmetoken"),
+        }
+    };
+
+    $http(request).then(function successCallback(response) {
+        var data = angular.fromJson(response.data);
+        var buy = data.Buy;
+        var sell = data.Sell;
+        $scope.sellAccept = sell.Accept;
+        $scope.sellDone = sell.Done;
+        $scope.sellRefuse = sell.Refuse;
+        $scope.sellCancel = sell.Cancel;
+        $scope.sellFinish = sell.Finish;
+        $scope.buyAccept = buy.Accept;
+        $scope.buyDone = buy.Done;
+        $scope.buyRefuse = buy.Refuse;
+        $scope.buyCancel = buy.Cancel;
+
+    }, function errorCallback(response) {
+
+        console.log(response);
+        
+        if (response.statusText == "Not Found"){
+            $scope.requestError = "Veuillez vérifier votre identifiant et votre mot de passe";                
+        }
+        
+    });
+
+}]);
